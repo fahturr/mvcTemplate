1e5cf8d9eec9e67106c72cc4f91cd61d56638f68
fahturr <fatur2077@gmail.com>
2019-07-13 21:26:31 +0700

    Nambah paragraf

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -2,4 +2,6 @@
     <h1 class="mt-4">About Me</h1>
     <img src="<?= BASEURL; ?>/img/profile1.JPG" alt="Ardy Junata" width="200">
     <p>Hallo nama saya <?= $data['nama']; ?>, Umur saya <?= $data['umur']; ?> tahun, saya adalah seorang <?= $data['pekerjaan']; ?></p>
+
+    <p>Ini adalah perubahannya</p>
 </div>
